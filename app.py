aeafcbcfa83079ba90e5e7fd1a1e7acfb518f21b
Hendrik_Berlin <hello@niefeld.com>
2018-11-17 09:53:49 +0100

    New web client

--- app.py
+++ app.py
@@ -25,29 +25,11 @@ def imprint():
 def privacy():
     return render_template('privacy.html')
 
-@app.route("/01")
-def index01():
-    return render_template('01.html')
+@app.route("/landing")
+def landing():
+    return render_template('landing.html')
 
-@app.route("/02")
-def index02():
-    return render_template('02.html')
 
-@app.route("/03")
-def index03():
-    return render_template('03.html')
-
-@app.route("/04")
-def index04():
-    return render_template('04.html')
-
-@app.route("/05")
-def index05():
-    return render_template('05.html')
-
-@app.route("/06")
-def index06():
-    return render_template('06.html')
 
 #-------------------------------------------------------------
 # standard boilerplate
